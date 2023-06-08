03057ad6fbdf2ffb451dc60011c3878a218b3427
dummy-co-der <maheshwarianuj1001@gmail.com>
2023-06-08 22:12:16 +0530

    redux setup, header & footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.scss";
-import { Provider } from "react-redux";
-import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
