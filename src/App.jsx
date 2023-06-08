cc7c9dca1fa85fd684cd62f89bd6ce7aac453117
dummy-co-der <maheshwarianuj1001@gmail.com>
2023-06-09 01:53:14 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration, getGenres } from "./store/homeSlice";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 // import Details from "./pages/details/Details";
 // import SearchResult from "./pages/searchResult/SearchResult";
 // import Explore from "./pages/explore/Explore";
@@ -56,13 +56,13 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      {/* <Routes>
+      <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/:mediaType/:id" element={<Details />} />
+                {/* <Route path="/:mediaType/:id" element={<Details />} />
                 <Route path="/search/:query" element={<SearchResult />} />
                 <Route path="/explore/:mediaType" element={<Explore />} />
-                <Route path="*" element={<PageNotFound />} />
-            </Routes> */}
+                <Route path="*" element={<PageNotFound />} /> */}
+            </Routes>
       <Footer />
     </BrowserRouter>
   );
